d95884ea288d2e6de185b7df9f52dee34e06acc1
JOOWAN HAN <joowan@seoultech.ac.kr>
2026-01-01 00:50:39 +0900

    feat: 커스텀 구분자 처리

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -1,6 +1,18 @@
 package calculator
 
 class Calculator(val line: String) {
-
-
+	
+	val endCustomDelimiters = line.indexOf("\n")
+	var delimiters = mutableListOf(',', ':')
+	var numbersString = ""
+	lateinit var numbers: List<Int>
+	
+	fun parseCustomDelimiters() {
+		// 커스텀 구분자(//와 \n 사이) 추출 로직
+		if (line.startsWith("//")) {
+			val customDelimiters = line.substring(2, endCustomDelimiters).map { it.toChar() }
+			delimiters.addAll(customDelimiters)
+		}
+	}
+	
 }
