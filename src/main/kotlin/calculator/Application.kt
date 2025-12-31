c425bae9d5bd063849c2114d23e9d3dcfdc035f1
JOOWAN HAN <joowan@seoultech.ac.kr>
2026-01-01 00:53:51 +0900

    feat: 결과 출력하기

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -2,9 +2,18 @@ package calculator
 
 fun main() {
     // TODO: 프로그램 구현
-	println("덧셈할 문자열을 입력해 주세요.")
-	var line = camp.nextstep.edu.missionutils.Console.readLine()
-	Calculator(line)
+	
+	
+		println("덧셈할 문자열을 입력해 주세요.")
+		var line = camp.nextstep.edu.missionutils.Console.readLine()
+		val result = Calculator(line).calculate()
+		println("결과: $result")
+	
+
+
+	
+	
+	
 	
 	
 	
