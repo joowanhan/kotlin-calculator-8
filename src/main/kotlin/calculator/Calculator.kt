c1f3c8bba4531d8cef4084177ad93d5050b46448
JOOWAN HAN <joowan@seoultech.ac.kr>
2026-01-02 18:08:03 +0900

    refactor(Calculator): 이스케이프 문자 처리 및 구조 변경 - 이스케이프 문자 처리: 정규표현식 사용 - 구조 변경: 커스텀 구분자 처리를 함수 내로 이동하여 안전하게 실행되게 함

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -2,32 +2,39 @@ package calculator
 
 class Calculator(val line: String) {
 	
-	val endCustomDelimiters = line.indexOf("\n")
 	var delimiters = mutableListOf(',', ':')
-	var numbersString = ""
-	lateinit var numbers: List<Int>
+	var numbersString = line // 커스텀 구분자가 없는 평범한 입력(예: 1,2:3)"을 처리해야 하기 위해 초기화
 	
 	fun parseCustomDelimiters() {
 		// 커스텀 구분자(//와 \n 사이) 추출 로직
 		if (line.startsWith("//")) {
-			val customDelimiters = line.substring(2, endCustomDelimiters).map { it.toChar() }
-			delimiters.addAll(customDelimiters)
+			// 정규표현식을 이용해 커스텀구분자(들)을 분리
+			// (?: ... )는 비캡처 그룹, groupValues 인덱스 순서 영향 X
+			// (?:\\n|\n)을 사용하여 '문자열 \n'과 '실제 줄바꿈' 모두 매칭
+			val regex = Regex("""^//(.*)(?:\n|\\n)(.*)$""")
+			val matchResult = regex.find(line)
+			if (matchResult != null) {
+				// 숫자 문자열 저장
+				numbersString = matchResult.groupValues[2]
+				// 커스텀 구분자 저장 (문자열을 문자 리스트로 즉시 변환)
+				val customDelimiters = matchResult.groupValues[1].toList()
+				delimiters.addAll(customDelimiters)
+				
+			}
+			
 		}
 	}
 	
-	fun parseNumbers() {
-		numbersString = line.substring(endCustomDelimiters + 1)
-		numbers = numbersString.split(*delimiters.toCharArray()).filter { it.isNotBlank() } // (권장) 연속된 구분자로 인한 빈 문자열 제거
+	fun parseNumbers(): List<Int> {
+		return numbersString.split(*delimiters.toCharArray()).filter { it.isNotBlank() } // (권장) 연속된 구분자로 인한 빈 문자열 제거
 			.map { it.trim().toInt() }  // 공백 제거 후 정수로 변환
 	}
 	
 	fun calculate(): Int {
-		var sum = 0
 		parseCustomDelimiters()
-		parseNumbers()
-		require(numbers.all { it >= 0 })
-		numbers.map { sum += it }
-		return sum
+		val numbers = parseNumbers()
+		require(numbers.all { it >= 0 }) { "negative integers can't be calculated" }
+		return numbers.sum()
 		
 	}
 	
